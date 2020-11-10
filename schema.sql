60df9e796e415f5a897324ff5d0ba3fa66ab87f2
Jay Idrees <idreesjay@gmail.com>
2020-11-10 13:03:02 -0500

    specified user category  variables for MYSQL database

---
+++ schema.sql
@@ -0,0 +1,6 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS rememberDat_local;
+-- Creates the "blogger" database --
+CREATE DATABASE rememberDat_local;
+
+SELECT * FROM rememberdat_local.users;
