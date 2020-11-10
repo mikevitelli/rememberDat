efcf6fab292e43c1eb745c61f19e0574b5d6309a
Mike <mike@mikevitelli.com>
2020-11-10 16:45:32 -0500

    completed initial routing data

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -26,6 +26,10 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
+  app.get("/content-landing", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/content-landing.html"));
+  });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/categories", isAuthenticated, (req, res) => {
