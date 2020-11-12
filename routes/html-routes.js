7f43c21df1351b26a241828d28679b71f1684a5d
Mike <mike@mikevitelli.com>
2020-11-12 11:56:23 -0500

    fixed paths

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,18 +7,12 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function (app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the content landing page
-    if (req.user) {
-      res.redirect("/content-landing.html");
-    }
-    // if not take them to the signup page
+    // if not take them to the start page
     res.sendFile(path.join(__dirname, "../pages/startpage.html"));
   });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/content-landing.html");
-    }
     res.sendFile(path.join(__dirname, "../pages/login.html"));
   });
 
