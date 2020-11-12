2b9b6cef2a853fc31724eeaf269ae9195a644fac
rrrossettiii <rrrossettiii@gmail.com>
2020-11-12 12:58:19 -0500

    12 12

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,17 +1,25 @@
 $(document).ready(() => {
+  console.log("categories.js loaded");
   // getting references to form input
   const nextBtn = $("button#catNextBtn");
   const backBtn = $("button#catBackBtn");
 
   //   when next button is pressed, content landing page is displayed
 
-  nextBtn.on("click", (event) => {
+  nextBtn.on("click", event => {
     event.preventDefault();
     window.location.replace("/content-landing");
   });
 
-  backBtn.on("click", (event) => {
+  backBtn.on("click", event => {
     event.preventDefault();
     window.location.replace("/startpage");
+  });
+});
+
+$(".btn").each(function() {
+  $(this).on("click", function() {
+    $(this).addClass("btn-success");
+    $(this).removeClass("button-danger");
   });
 });
