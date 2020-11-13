26062b4fa26997a1583cc5062e0484190efb224a
Jay Idrees <idreesjay@gmail.com>
2020-11-13 11:30:47 -0500

    updated the code for post requests

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -91,26 +91,26 @@ console.log('user', req.user)
 
  
 
-  
+
 
   })
 });
 
 
- // POST route for saving a new todo
- app.post("/api/todos", function(req, res) {
-  console.log(req.body);
-  // create takes an argument of an object describing the item we want to
-  // insert into our table. In this case we just we pass in an object with a text
-  // and complete property (req.body)
-  db.Todo.create({
-    text: req.body.text,
-    complete: req.body.complete
-  }).then(function(dbTodo) {
-    // We have access to the new todo as an argument inside of the callback function
-    res.json(dbTodo);
-  });
-});
+//  // POST route for saving a new todo
+//  app.post("/api/todos", function(req, res) {
+//   console.log(req.body);
+//   // create takes an argument of an object describing the item we want to
+//   // insert into our table. In this case we just we pass in an object with a text
+//   // and complete property (req.body)
+//   db.Todo.create({
+//     text: req.body.text,
+//     complete: req.body.complete
+//   }).then(function(dbTodo) {
+//     // We have access to the new todo as an argument inside of the callback function
+//     res.json(dbTodo);
+//   });
+// });
 
 
 
